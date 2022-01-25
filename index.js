482abda73e2df4de27e5e13f8ae155c29b55a135
Aline Cristina <bertola.aline@gmail.com>
2022-01-25 20:37:19 -0300

    Cria bilhetes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Eu sou a Aline!")
+const quantidadeDeBilhetes = 1000
+const numerosDiv = document.querySelector("#numeros")
+
+for (let i = 0; i < quantidadeDeBilhetes; i++) {
+  const element = document.createElement("div")
+  element.innerText = i + 1
+  element.setAttribute("class", "bilhete")
+  numerosDiv.appendChild(element)
+}
