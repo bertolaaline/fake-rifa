9fd1aab288d47ed6880a77c61a2c25c4afc465fb
Aline Cristina <bertola.aline@gmail.com>
2022-01-25 22:06:57 -0300

    Altera status

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const quantidadeDeBilhetes = 1000
 const numerosDiv = document.querySelector("#numeros")
 
-for (let i = 0; i < quantidadeDeBilhetes; i++) {
-  const element = document.createElement("div")
-  element.innerText = i + 1
-  element.setAttribute("class", "bilhete")
-  numerosDiv.appendChild(element)
+const bilhetes = []
+
+const geraBilhetes = () => {
+  for (let i = 0; i < quantidadeDeBilhetes; i++) {
+    const bilhete = {
+      numero: i + 1,
+      status: "disponivel"
+    }
+    bilhetes.push(bilhete)
+  }
 }
+
+const montaTela = () => {
+  numerosDiv.innerHTML = ""
+
+  for (const bilhete of bilhetes) {
+    const element = document.createElement("div")
+    element.innerText = bilhete.numero
+    element.setAttribute("id", `bilhete-${bilhete.numero}`)
+    element.setAttribute("class", "bilhete")
+    element.classList.add(bilhete.status)
+    numerosDiv.appendChild(element)
+  }  
+}
+
+const reservar = numeroBilhete => {
+  bilhetes[numeroBilhete - 1].status = "reservado"
+  montaTela()
+}
+
+geraBilhetes()
+montaTela()
+
+numerosDiv.addEventListener("click", e => {
+  if(e.target.id.includes("bilhete")) {
+    const numeroBilhete = e.target.id.split("-")[1]
+    reservar(numeroBilhete)
+  }
+})
